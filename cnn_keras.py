0f2c3a46b4a0f70ec14929a412ee3e5558375b17
vermouth1992 <czhangseu@gmail.com>
2017-03-11 09:50:30 +0800

    model ensemble

--- cnn_keras.py
+++ cnn_keras.py
@@ -5,97 +5,153 @@ It gets down to 0.65 test logloss in 25 epochs, and down to 0.55 after 50 epochs
 (it's still underfitting at that point, though).
 '''
 
-from __future__ import print_function
-from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
+from keras.regularizers import l2
+from keras.optimizers import Adam
+
+from data_utils import get_CIFAR10_data
+
+import numpy as np
 
 batch_size = 128
 nb_classes = 10
-nb_epoch = 200
-data_augmentation = False
+nb_epoch = 10
 
 # input image dimensions
 img_rows, img_cols = 32, 32
 # The CIFAR10 images are RGB.
 img_channels = 3
 
+reg = 0
+
 # The data, shuffled and split between train and test sets:
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-print('X_train shape:', X_train.shape)
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
+data = get_CIFAR10_data()
+X_train = data['X_train']
+y_train = data['y_train']
+X_val = data['X_val']
+y_val = data['y_val']
+X_test = data['X_test']
+y_test = data['y_test']
 
 # Convert class vectors to binary class matrices.
 Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+Y_val = np_utils.to_categorical(y_val, nb_classes)
 
-model = Sequential()
 
-model.add(Convolution2D(32, 3, 3, border_mode='same',
-                        input_shape=X_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(Convolution2D(32, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+def create_model_1():
+    model = Sequential()
 
-model.add(Convolution2D(64, 3, 3, border_mode='same'))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+    model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=X_train.shape[1:], W_regularizer=l2(reg),
+                            name='conv1'))
+    model.add(BatchNormalization(axis=1, name='conv1_batch'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
 
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
+    model.add(Flatten())
+    model.add(Dense(512, W_regularizer=l2(reg), name='fc1'))
+    model.add(BatchNormalization(name='fc1_batch'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
 
-# Let's train the model using RMSprop
-model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+    model.add(Dense(nb_classes, W_regularizer=l2(reg), name='fc2'))
+    model.add(Activation('softmax'))
 
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+    return model
 
-if not data_augmentation:
-    print('Not using data augmentation.')
+
+def create_model_2():
+    model = Sequential()
+
+    model.add(Convolution2D(16, 3, 3, border_mode='same', input_shape=X_train.shape[1:], W_regularizer=l2(reg),
+                            name='conv1'))
+    model.add(BatchNormalization(axis=1, name='conv1_batch'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(32, 3, 3, border_mode='same', W_regularizer=l2(reg), name='conv2'))
+    model.add(BatchNormalization(axis=1, name='conv2_batch'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512, W_regularizer=l2(reg), name='fc1'))
+    model.add(BatchNormalization(name='fc1_batch'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(nb_classes, W_regularizer=l2(reg), name='fc2'))
+    model.add(Activation('softmax'))
+
+    return model
+
+
+def train_model():
+    model_index = 2
+
+    model = create_model_2()
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr=1e-3),
+                  metrics=['accuracy'])
+
+    weight_file_name = 'cifar10_cnn_weights_%d.h5' % (model_index)
+
+    try:
+        model.load_weights(weight_file_name)
+    except:
+        pass
+
     model.fit(X_train, Y_train,
               batch_size=batch_size,
               nb_epoch=nb_epoch,
-              validation_data=(X_test, Y_test),
+              validation_data=(X_val, Y_val),
               shuffle=True)
-else:
-    print('Using real-time data augmentation.')
-    # This will do preprocessing and realtime data augmentation:
-    datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
 
-    # Compute quantities required for featurewise normalization
-    # (std, mean, and principal components if ZCA whitening is applied).
-    datagen.fit(X_train)
+    model.save_weights(weight_file_name)
 
-    # Fit the model on the batches generated by datagen.flow().
-    model.fit_generator(datagen.flow(X_train, Y_train,
-                                     batch_size=batch_size),
-                        samples_per_epoch=X_train.shape[0],
-                        nb_epoch=nb_epoch,
-                        validation_data=(X_test, Y_test))
+    predicted_labels = model.predict_classes(X_test, verbose=0)
+    accuracy = np.mean((predicted_labels == y_test).astype(np.float32))
+    print 'Test accuracy:', accuracy
+
+
+def model_ensemble():
+
+    def check_accuracy(score, y):
+        return np.mean((np.argmax(score, axis=1) == y).astype(np.float32))
+
+    model_1 = create_model_1()
+    model_1.load_weights('cifar10_cnn_weights_1.h5')
+    model_2 = create_model_2()
+    model_2.load_weights('cifar10_cnn_weights_2.h5')
+
+    print '========Ensemble validation data========'
+    score_1 = model_1.predict(X_val, verbose=0)
+    acc_1 = check_accuracy(score_1, y_val)
+    score_2 = model_2.predict(X_val, verbose=0)
+    acc_2 = check_accuracy(score_2, y_val)
+    avg_score = np.add(score_1, score_2) / 2
+    avg_acc = check_accuracy(avg_score, y_val)
+    print 'model 1 acc', acc_1
+    print 'model 2 acc', acc_2
+    print 'ensemble acc', avg_acc
+    print '========Ensemble test data========'
+    score_1 = model_1.predict(X_test, verbose=0)
+    acc_1 = check_accuracy(score_1, y_test)
+    score_2 = model_2.predict(X_test, verbose=0)
+    acc_2 = check_accuracy(score_2, y_test)
+    avg_score = np.add(score_1, score_2) / 2
+    avg_acc = check_accuracy(avg_score, y_test)
+    print 'model 1 acc', acc_1
+    print 'model 2 acc', acc_2
+    print 'ensemble acc', avg_acc
+
+
+if __name__ == '__main__':
+    model_ensemble()
