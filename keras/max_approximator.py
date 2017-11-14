cbd47d976df3a627cee9561dca5884ea6348f66b
vermouth1992 <czhangseu@gmail.com>
2017-11-14 11:21:04 -0800

    1500 sample is sufficient for 4 class argmax

--- keras/max_approximator.py
+++ keras/max_approximator.py
@@ -3,13 +3,13 @@ Use a 3 layer perceptron to simulate a max function over a vector
 """
 
 import numpy as np
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, BatchNormalization
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.utils import np_utils
 
 
-nb_classes = 16
+nb_classes = 4
 
 def normalize(data):
     max_data = np.max(data, axis=1)
@@ -25,9 +25,9 @@ def create_dataset(num_samples, num_features):
 
 def create_network():
     model = Sequential()
-    model.add(Dense(512, input_shape=(nb_classes,)))
+    model.add(Dense(128, input_shape=(nb_classes,)))
     model.add(Activation('relu'))
-    model.add(Dense(512))
+    model.add(Dense(128))
     model.add(Activation('relu'))
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
@@ -38,11 +38,11 @@ def create_network():
 
 
 if __name__ == '__main__':
-    (training_data, training_labels), (testing_data, testing_labels) = create_dataset(50000, nb_classes)
+    (training_data, training_labels), (testing_data, testing_labels) = create_dataset(1500, nb_classes)
     training_labels = np_utils.to_categorical(training_labels, nb_classes)
     testing_labels = np_utils.to_categorical(testing_labels, nb_classes)
     model = create_network()
-    model.fit(training_data, training_labels, batch_size=128, epochs=10,
+    model.fit(training_data, training_labels, batch_size=128, epochs=100,
               validation_data=(testing_data, testing_labels), shuffle=True)
 
     data = np.random.randn(4000, nb_classes)
