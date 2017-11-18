9f8073465969f7137cd87d6c7f54f8da070d5816
vermouth1992 <czhangseu@gmail.com>
2017-11-18 11:14:15 -0800

    test ddpg on Pendulum

--- deep-rl/environment_playground.py
+++ deep-rl/environment_playground.py
@@ -1,12 +1,13 @@
 import gym
 
-env = gym.make('CartPole-v0')
+env = gym.make('Pendulum-v0')
 for i_episode in range(20):
     observation = env.reset()
     for t in range(100):
         env.render()
-        print(observation)
+        print('Obs: {}'.format(observation))
         action = env.action_space.sample()
+        print('Action: {}'.format(action))
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t + 1))
