5ab09bbf12957f835bf7602358ab82d190449856
vermouth1992 <czhangseu@gmail.com>
2018-03-16 00:56:32 -0700

    cgan on mnist

--- gan/cgan.py
+++ gan/cgan.py
@@ -4,6 +4,8 @@ Condition GAN based on DCGAN architecture
 
 from __future__ import print_function
 
+import tensorflow as tf
+
 import keras
 import numpy as np
 from keras.initializers import RandomNormal
@@ -11,16 +13,16 @@ from keras.layers import Conv2D, Conv2DTranspose, LeakyReLU, BatchNormalization,
     Activation, Input
 from keras.models import Model
 from keras.optimizers import RMSprop
+from keras import backend as K
 
 
 class CGAN(object):
-    def __init__(self):
-        # config
-        self.code_size = 64
-        self.learning_rate = 1e-4
-        self.batch_size = 32
-        self.num_epoch = 5
-        self.log_step = 50
+    def __init__(self, code_size=64, learning_date=1e-4, batch_size=32, num_channels=3):
+        # config, the image size is assume to be 32 * 32
+        self.code_size = code_size
+        self.learning_rate = learning_date
+        self.batch_size = batch_size
+        self.num_channels = num_channels
 
         self.generator = self._create_generator()
         self.generator.compile(loss='binary_crossentropy', optimizer=RMSprop(self.learning_rate))
@@ -31,8 +33,16 @@ class CGAN(object):
         self.discriminator_generator = self._combine_generator_discriminator()
         self.discriminator_generator.compile(loss='binary_crossentropy', optimizer=RMSprop(self.learning_rate))
 
+        # setup tensorboard visualization
+        self.dis_loss = tf.Variable(0.)
+        tf.summary.scalar('dis_loss', self.dis_loss)
+        self.gen_loss = tf.Variable(0.)
+        tf.summary.scalar('gen_loss', self.gen_loss)
+        self.summary_ops = tf.summary.merge_all()
+
+
     def _create_discriminator(self):
-        image_input = Input(shape=(32, 32, 3), name='image_input')
+        image_input = Input(shape=(32, 32, self.num_channels), name='image_input')
         x = Conv2D(filters=32, kernel_size=(4, 4), strides=(2, 2), padding='same',
                    kernel_initializer=RandomNormal(0, 0.02))(image_input)
         x = LeakyReLU(alpha=0.2)(x)
@@ -78,7 +88,7 @@ class CGAN(object):
                             kernel_initializer=RandomNormal(0, 0.02))(x)
         x = BatchNormalization()(x)
         x = LeakyReLU(alpha=0.2)(x)
-        x = Conv2DTranspose(filters=3, kernel_size=(4, 4), strides=(2, 2), padding='same',
+        x = Conv2DTranspose(filters=self.num_channels, kernel_size=(4, 4), strides=(2, 2), padding='same',
                             kernel_initializer=RandomNormal(0, 0.02))(x)
         output = Activation('sigmoid')(x)
         model = Model(inputs=[noise_input, label_input], outputs=output)
@@ -92,7 +102,7 @@ class CGAN(object):
         model = Model(inputs=[noise_input, label_input], outputs=output)
         return model
 
-    def train(self, train_samples, training_labels):
+    def train(self, train_samples, training_labels, num_epoch=5, log_step=50, verbose=True, summary_path='./summary/cgan'):
         num_train = train_samples.shape[0]
         step = 0
 
@@ -104,7 +114,11 @@ class CGAN(object):
 
         dis_losses = []
         gen_losses = []
-        for epoch in range(self.num_epoch):
+
+        session = K.get_session()
+        writer = tf.summary.FileWriter(summary_path, session.graph)
+
+        for epoch in range(num_epoch):
             for i in range(num_train // self.batch_size):
                 step += 1
                 # get image
@@ -130,13 +144,22 @@ class CGAN(object):
 
                 gen_loss = self.discriminator_generator.train_on_batch([noise, sampled_labels], [1] * self.batch_size)
 
+                # use tensorboard to visualize
+                summary_str = session.run(self.summary_ops, feed_dict={
+                    self.dis_loss: dis_loss,
+                    self.gen_loss: gen_loss
+                })
+
+                writer.add_summary(summary_str, step)
+                writer.flush()
+
                 plot_dis_s = plot_dis_s * smooth_factor + dis_loss * (1 - smooth_factor)
                 plot_gen_s = plot_gen_s * smooth_factor + gen_loss * (1 - smooth_factor)
                 plot_ws = plot_ws * smooth_factor + (1 - smooth_factor)
                 dis_losses.append(plot_dis_s / plot_ws)
                 gen_losses.append(plot_gen_s / plot_ws)
 
-                if step % self.log_step == 0:
+                if step % log_step == 0 and verbose:
                     print('Iteration {0}: dis loss = {1:.4f}, gen loss = {2:.4f}'.format(step, dis_loss, gen_loss))
         return dis_losses, gen_losses
 
---
+++ gan/rcgan.py
@@ -0,0 +1,10 @@
+"""
+Recurrent Conditional GAN in keras
+"""
+
+class RCGAN(object):
+    def __init__(self):
+        pass
+
+    def _create_discriminator(self):
+        pass
